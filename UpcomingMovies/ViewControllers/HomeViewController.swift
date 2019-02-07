b2cc37d96bb19c91f2d95da56e54b638adc10d35
Erika Bueno <erikacb@gmail.com>
2019-02-06 22:57:05 -0200

    embed view controller in navigation controller and add basic structure to main storyboard

---
+++ UpcomingMovies/ViewControllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  UpcomingMovies
+//
+//  Created by Erika Bueno on 06/02/19.
+//  Copyright © 2019 8sys. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
