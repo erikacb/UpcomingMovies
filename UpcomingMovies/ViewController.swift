c975b765cd13ef2483ac57a1dff229726f245e50
Erika Bueno <erikacb@gmail.com>
2019-02-06 22:18:12 -0200

    Initial Commit

---
+++ UpcomingMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpcomingMovies
+//
+//  Created by Erika Bueno on 06/02/19.
+//  Copyright © 2019 8sys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
